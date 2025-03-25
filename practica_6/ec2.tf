7b65c15a710349b2aed0c8a465d0b4a56e5cb8b2
Ale1120 <alejandro.ortegano11@gmail.com>
2025-03-25 21:39:57 +0100

    data y targert para aplicar un recurso y tener los datos de una recurso ya creado desde una nube

--- practica_6/ec2.tf
+++ practica_6/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "public_instance" {
     ami = "ami-084568db4383264d4"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.public_subnet_virginia.id
+    key_name = data.aws_key_pair.key.key_name
     tags = {
         Name = "Ubuntu"
     }
--- practica_6/vcp.tf
+++ practica_6/vcp.tf
@@ -11,7 +11,7 @@ resource "aws_subnet" "public_subnet_virginia" {
     cidr_block = var.subnets[0]
     map_public_ip_on_launch = true
     tags = {
-        Name = "Public subnet"
+        Name = "Public subnet_cambio en un recurso con terraform --target (aws_subnet.public_subent_virginia)name recurso"
     }
     
 }
@@ -20,7 +20,10 @@ resource "aws_subnet" "private_subnet_virginia" {
     vpc_id = aws_vpc.vpc_virgina.id
     cidr_block = var.subnets[1] 
     tags = {
-        Name = "Private subnet"
+        Name = "Private subnetl"
     }
+    depends_on = [ 
+        aws_subnet.public_subnet_virginia
+     ]
 }
 
